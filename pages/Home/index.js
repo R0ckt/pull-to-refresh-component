f645beaddee70e59d0dfc9fc4283b3413024b72a
tsmsp <tsm_sp@hotmail.com>
2022-11-15 15:16:42 -0800

    added pages

---
+++ pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Hello, I am your Home!</Text>
+    </View>
+  );
+}
+
+export default Home;
