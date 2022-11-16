3007c230838eabba8da6237b6a9eccba9c4b59ca
tsmsp <tsm_sp@hotmail.com>
2022-11-15 19:00:35 -0800

    babel presets for React Native applications

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    "presets": ["module:metro-react-native-babel-preset"],    
+    plugins: [      
+      [
+        'module-resolver',
+        {          
+          alias: {
+            '@src': './src',
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@assets': './src/assets',            
+          },          
+        },
+      ],
+    ],
   };
+
+  
 };
+
+
